e4b01089c8416661670549090c8d3071762df45b
Felix Boy <felix.boy@th-nuernberg.de>
2026-03-17 19:31:37 +0100

    adapted plot codes to use pylectureplots package.

--- EME2/Lecture_2_Displacement_Field_Animation.py
+++ EME2/Lecture_2_Displacement_Field_Animation.py
@@ -1,9 +1,11 @@
 import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
-pio.templates.default = "none"
 from plotly.subplots import make_subplots
+pio.templates.default = "none"
+import pylectureplots as pylp
 from pathlib import Path
+output_path = Path(__file__).resolve().with_suffix('.html')
 
 
 
@@ -162,58 +164,5 @@ for annotation in fig['layout']['annotations']:
 
 fig.update_layout(margin=dict(l=50, r=50, t=50,b=50),autosize=True,font=dict(size=16),title_font=dict(size=18),sliders=sliders)
 
-# Save html
-html = fig.to_html(
-    full_html=True,
-    include_plotlyjs='cdn',     # smaller file; loads Plotly from CDN
-    config = {"responsive": True},
-    include_mathjax='cdn'
-)
-
-# Wrap it in a minimal full-screen HTML template
-
-fullscreen_html = f"""<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width,initial-scale=1" />
-<style>
-  html, body {{
-    height: 100%;
-    width: 100%;
-    margin: 0;     /* remove default margins */
-    padding: 0;
-    overflow: hidden;
-    background: #fff;
-  }}
-  
-#wrap {{
-        height: 100dvh;   /* works inside PowerPoint iframe */
-        width: 100vw;
-    }}
-
-#wrap > div {{
-        height: 100% !important;
-        width: 100% !important;
-    }}
-
-.plotly-graph-div {{
-        height: 100% !important;
-        width: 100% !important;
-    }}
-
-</style>
-</head>
-<body>
-    
-<div id="wrap">
-        {html}
-    </div>
-
-</body>
-</html>"""
-
-output_path = Path(__file__).resolve().with_suffix('.html')
-with open(output_path, "w", encoding="utf-8") as f:
-    f.write(fullscreen_html)
-
+# Export to html
+pylp.to_html(fig,output_path)
