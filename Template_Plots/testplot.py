d19bab564446216023ca07aaa4bd9d6c113ff4eb
Felix Boy <felix.boy@th-nuernberg.de>
2026-03-17 19:16:14 +0100

    added the to_html code to pylectureplots package

--- Template_Plots/testplot.py
+++ Template_Plots/testplot.py
@@ -2,7 +2,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
 pio.templates.default = "none"
-from pathlib import Path
+import pylectureplots as pylp
 
 fig = go.Figure()
 x = np.arange(10)
@@ -10,60 +10,4 @@ fig.add_trace(go.Scatter(x=x,y=x));
 
 fig.update_layout(margin=dict(l=25, r=10, t=10,b=40),autosize=True)
 
-
-
-# Save html
-html = fig.to_html(
-    full_html=True,
-    include_plotlyjs='cdn',     # smaller file; loads Plotly from CDN
-    config = {"responsive": True},
-    include_mathjax='cdn'
-)
-
-# Wrap it in a minimal full-screen HTML template
-
-fullscreen_html = f"""<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width,initial-scale=1" />
-<style>
-  html, body {{
-    height: 100%;
-    width: 100%;
-    margin: 0;     /* remove default margins */
-    padding: 0;
-    overflow: hidden;
-    background: #fff;
-  }}
-  
-#wrap {{
-        height: 100dvh;   /* works inside PowerPoint iframe */
-        width: 100vw;
-    }}
-
-#wrap > div {{
-        height: 100% !important;
-        width: 100% !important;
-    }}
-
-.plotly-graph-div {{
-        height: 100% !important;
-        width: 100% !important;
-    }}
-
-</style>
-</head>
-<body>
-    
-<div id="wrap">
-        {html}
-    </div>
-
-</body>
-</html>"""
-
-output_path = Path(__file__).resolve().with_suffix('.html')
-with open(output_path, "w", encoding="utf-8") as f:
-    f.write(fullscreen_html)
-
+pylp.to_html(fig)
--- pylectureplots/src/pylectureplots/export_utils.py
+++ pylectureplots/src/pylectureplots/export_utils.py
@@ -1,2 +1,57 @@
-def to_html(x):
-    print(x)
+from pathlib import Path
+
+def to_html(fig):
+    # Save html
+    html = fig.to_html(
+        full_html=True,
+        include_plotlyjs='cdn',     # smaller file; loads Plotly from CDN
+        config = {"responsive": True},
+        include_mathjax='cdn'
+    )
+
+    # Wrap it in a minimal full-screen HTML template
+
+    fullscreen_html = f"""<!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <style>
+        html, body {{
+            height: 100%;
+            width: 100%;
+            margin: 0;     /* remove default margins */
+            padding: 0;
+            overflow: hidden;
+            background: #fff;
+        }}
+  
+        #wrap {{
+            height: 100dvh;   /* works inside PowerPoint iframe */
+            width: 100vw;
+        }}
+
+        #wrap > div {{
+            height: 100% !important;
+            width: 100% !important;
+        }}
+
+        .plotly-graph-div {{
+            height: 100% !important;
+            width: 100% !important;
+        }}
+
+    </style>
+    </head>
+    <body>
+    
+    <div id="wrap">
+        {html}
+    </div>
+
+    </body>
+    </html>"""
+
+    output_path = Path(__file__).resolve().with_suffix('.html')
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(fullscreen_html)
